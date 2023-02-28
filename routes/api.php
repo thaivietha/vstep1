654c377d5fb13b38cd249d39ebaac5abf905e7ad
Administrator <sonvt.vietmmo@gmail.com>
2023-02-28 15:27:51 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -124,9 +124,9 @@ Route::group(['namespace'=>'v1'],function (){
     Route::get('/baihocs/getzipfile/{idBaihoc}', 'ApiController@getzipfileIdLesson');
 
     Route::group([
-//        'middleware' => 'auth:api'
+        'middleware' => 'auth:api'
     ],function () {
-        Route::post('Account/ChangePassword', 'ApiController@ChangePassword');
+        Route::post('/Account/ChangePassword/{userName}', 'ApiController@ChangePassword');
         Route::get('/khoahocs/{userName}', 'ApiController@coursesByUser');
         Route::get('/baihocs/get/{userName}', 'ApiController@lessonsByUser');
     });
